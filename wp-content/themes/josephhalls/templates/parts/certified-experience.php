8120d12802e001bc91942dca608d1d11a80fa78b
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-09-20 13:09:07 +0100

    styling tweaks

--- wp-content/themes/josephhalls/templates/parts/certified-experience.php
+++ wp-content/themes/josephhalls/templates/parts/certified-experience.php
@@ -1,11 +1,12 @@
 <section id="certifications">
     <div class="certified-experience">
-        <h2>I solve problems and creating intuitive software solutions</h2>
-        <p> By creating seamless digital experiences that meet the needs of both users and businesses. I enjoy solving complex problems and building intuitive software solutions that make a real impact.</p>
+        <h2>I solve problems and create software solutions</h2>
+        <p> 
+        I focus on creating seamless digital experiences that align with both user needs and business objectives. I enjoy tackling complex challenges and building intuitive software solutions that deliver real impact.</p>
         
         <div class="experience-row">
-            <h3>With Certified experience</h3>
-            <p>I'm always exploring new technologies and certifications to stay ahead in the ever-evolving tech landscape.</p>
+            <h3>Certified experience</h3>
+            <p>Certifications to stay ahead in the ever-evolving tech landscape.</p>
             <div class="experience-grid">
                 <!-- Item One -->
                 <a href="https://www.open.ac.uk/courses/computing-it/degrees/bsc-computing-it-software-q62-soft" target="_blank">
